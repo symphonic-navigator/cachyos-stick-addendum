6a35e105d2575d6e006e0f58d72bdcb04be38f1a
symphonic-navigator <x@example.com>
2026-01-16 09:21:30 +0100

    made bootstrap.sh curl safe

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,19 @@
 #! /bin/bash
 
-# --- script setup ---
-set -euo pipefail
+{
+  # --- script setup ---
+  set -euo pipefail
 
-# --- bootstrapping ---
-installer_dir="$HOME/repos/cachyos-stick-addendum"
+  # --- bootstrapping ---
+  installer_dir="$HOME/repos/cachyos-stick-addendum"
 
-rm -rf "$installer_dir" || true
-mkdir -p "$installer_dir"
-git clone https://github.com/symphonic-navigator/cachyos-stick-addendum.git "$installer_dir"
+  rm -rf "$installer_dir" || true
+  mkdir -p "$installer_dir"
+  git clone https://github.com/symphonic-navigator/cachyos-stick-addendum.git "$installer_dir"
 
-# --- launch ---
-bash -c "$installer_dir/install.sh"
+  # --- launch ---
+  bash -c "$installer_dir/install.sh"
+
+  # --- clean exit ---
+  exit 0
+}
