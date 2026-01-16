1f31468eddee54d40217dc7d5aaf8a9ef614295e
symphonic-navigator <x@example.com>
2026-01-16 09:15:26 +0100

    added bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,10 +3,12 @@
 # --- script setup ---
 set -euo pipefail
 
+# --- bootstrapping ---
 installer_dir="$HOME/repos/cachyos-stick-addendum"
 
+rm -rf "$installer_dir" || true
 mkdir -p "$installer_dir"
-
 git clone https://github.com/symphonic-navigator/cachyos-stick-addendum.git "$installer_dir"
 
+# --- launch ---
 bash -c "$installer_dir/install.sh"
